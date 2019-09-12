23c12a05bcf800e78e53838d65fd0cfb0fcca337
vombagram <55178253+vombagram@users.noreply.github.com>
2019-09-12 16:04:00 -0400

    add smooth scrolling and logo animation

--- js/scripts.js
+++ js/scripts.js
@@ -11,39 +11,43 @@ var mr_firstSectionHeight,
 $(document).ready(function() { 
     "use strict";
  	$("#richmusic").addClass(" slide-in-from-top"); 
-    // Smooth scroll to inner links
-        var innerLinks = $('a.inner-link');
+    $("#richmusic").addClass("slide-in-from-top");
 
-        if(innerLinks.length){
-            innerLinks.each(function(){
-                var link = $(this);
-                var href = link.attr('href');
-                if(href.charAt(0) !== "#"){
-                    link.removeClass('inner-link');
-                }
-            });
-
-            var offset = 0;
-            if($('body[data-smooth-scroll-offset]').length){
-                offset = $('body').attr('data-smooth-scroll-offset');
-                offset = offset*1;
-            }
-            
-            smoothScroll.init({
-                selector: '.inner-link',
-                selectorHeader: null,
-                speed: 750,
-                easing: 'easeInOutCubic',
-                offset: offset
-            });
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
         }
-
-    // Update scroll variable for scrolling functions
-
-    addEventListener('scroll', function() {
-        mr_scrollTop = window.pageYOffset;
-    }, false);
-
+      }
+    });
     // Append .background-image-holder <img>'s as CSS backgrounds
 
     $('.background-image-holder').each(function() {
